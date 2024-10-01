61df1e04170922a7ea74eaeb0cb1e1e47c1c0bff
wincid-technology <wincidtechnology@gmail.com>
2024-10-01 18:11:44 +0530

    Update About.jsx

--- About.jsx
+++ About.jsx
@@ -7,7 +7,7 @@ const About = () => {
                 <div class="w-full justify-start items-center gap-8 grid lg:grid-cols-2 grid-cols-1">
                     <div class="w-full flex-col justify-start lg:items-start items-center gap-10 inline-flex">
                         <div class="w-full flex-col justify-start lg:items-start items-center gap-4 flex">
-                            <h2 class="text-gray-900 kanit-semibold-italic text-4xl font-bold font-manrope leading-normal lg:text-start text-center">About Who We Are</h2>
+                            <h2 class="text-gray-900 kanit-semibold-italic text-4xl font-bold font-manrope leading-normal lg:text-start text-center">Who We Are</h2>
                             <p class="text-black text-base kanit-light font-normal leading-relaxed lg:text-start text-center">Empowering businesses with top-tier packaging solutions, we are your
                                 go-to partner for all things corrugated. We've been at
                                 the forefront of delivering innovative, durable, and eco friendly
@@ -17,7 +17,7 @@ const About = () => {
                                 Trust us to protect what matters most. At Coropack, we specialize in creating high-quality, eco-friendly packaging designed to fit your unique products. Our solutions ensure maximum protection while minimizing environmental impact. Trust us to elevate your brand with packaging that delivers both style and sustainability. </p>
                         </div>
                     </div>
-                    <img class="lg:mx-0 mx-auto h-full rounded-3xl object-cover" src="" alt="about Us image" />
+                    <img class="lg:mx-0 mx-auto h-full rounded-tl-[100px] rounded-br-[100px] object-cover" src="/img/recycleCard.jpg" alt="about Us image" />
                 </div>
             </div>
         </section>
